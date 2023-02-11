9c5e333edb68662da6c2c6804476594f59922c03
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-02-11 20:05:29 +0530

    bug fixed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -50,11 +50,14 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(form),
-        });
+        const response = await fetch(
+          "https://dall-e-clone-4gkj.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(form),
+          }
+        );
 
         await response.json();
         navigate("/");
